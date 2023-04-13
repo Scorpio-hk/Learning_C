3fc4551a82384a4e1b82af9698366c39f5860944
Scorpio-hk <464445301@qq.com>
2023-04-13 17:37:43 +0800

    循环

---
+++ xunhuan/jishu.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=100;i++)
+        if (i%2 !=0 )
+        {
+            printf("%d  ",i);
+        }
+        
+}
+/*
+1进入循环
+p2 i=2
+p3 i=3
+p4 i=4
+    i=5 i++ i=6
+    
+
+
+*/
